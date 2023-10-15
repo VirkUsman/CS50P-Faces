d519e811aaebcff69c9f7c9d0d1bcf7b9ed6e61d
Usman Virk <usmanvirk11@gmail.com>
2023-10-15 15:20:12 +0200

    Final Commit

--- faces.py
+++ faces.py
@@ -1,12 +1,10 @@
 text = input("")
 emojis = {":)" : "🙂"  ,   ":(" : "🙁"  ,   ";)" : "😉"}
-# Maker a dictionary for emojies
 
-# print("Original Text : " + text) just for checking
 
 for sign in emojis.keys():
-    # keys() with get the only key values of dictionary of emojis
+
     text = text.replace(sign, emojis[sign])
-    # replace function will replace the text sign with emojies dictionary key:value.
+   
 
 print(text)
